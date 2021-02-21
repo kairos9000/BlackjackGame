547d85358b9083bb5ba9aef5972bcee313b4a086
Philip Bartmann <p.bartmann@oth-aw.de>
2021-02-21 12:12:49 +0100

    Stack of cards on the top right side added; next: animations for the cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style";
 import { ListOutput } from "./component/ListOutput";
 
+let transform = "4";
 document.querySelector("#startGameButton").addEventListener("click", (event) => {
     for (let i = 0; i < 10; i++) {
         let card = document.createElement("div");
@@ -8,13 +9,18 @@ document.querySelector("#startGameButton").addEventListener("click", (event) => 
 
         let cardFront = document.createElement("div");
         cardFront.classList.add("front");
-        cardFront.style.backgroundImage = "url('/assets/cards/KC.png')";
+        let q = "Q";
+        cardFront.style.backgroundImage = "url('/cards/" + q + "C.png')";
 
         let cardBack = document.createElement("div");
         cardBack.classList.add("back");
 
         card.appendChild(cardFront);
         card.appendChild(cardBack);
+
+        transform = parseInt(transform) + 4;
+        transform.toString();
+        card.style.transform = "translate(500px, -" + transform + "px) rotateZ(35deg)";
 
         document.getElementById("cardsAndBankSeat").appendChild(card);
     }
